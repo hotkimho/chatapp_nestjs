562aca18ea6837d5f538778f9b5bbd15447272bd
hokim <rlaghekd777@naver.com>
2022-02-07 19:56:51 +0900

    add template engine, env

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { join } from 'path';
+
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+  console.log(__dirname);
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
+  await app.listen(3001);
 }
+
 bootstrap();
