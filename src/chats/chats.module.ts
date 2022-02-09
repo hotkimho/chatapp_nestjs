ab675cf1bd2907dabdb95cf936e639b3165911f1
hotkimho <rlaghekd777@naver.com>
2022-02-10 02:35:46 +0900

    Feat: Add Db models

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ChatsGateway } from './chats.gateway';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Chatting, ChattingSchema } from './models/chattings.models';
+import { Socket, SocketSchema } from './models/sockets.model';
 
 @Module({
+  imports: [MongooseModule.forFeature([
+    { name: Chatting.name, schema: ChattingSchema },
+    { name: Socket.name, schema: SocketSchema },
+  ])],
   providers: [ChatsGateway],
 })
-export class ChatsModule {}
+export class ChatsModule {
+}
